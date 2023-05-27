2bae1b77b216265d6f6ca989a6fbbcfc53f0cf30
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-05-26 22:07:31 -0700

    login & logout hooks

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -6,11 +6,11 @@ export function useSignup() {
   const [isLoading, setIsLoading] = useState(null);
   const { dispatch } = useAuthContext();
 
-  const signup = async (email, password) => {
+  const login = async (email, password) => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("/api/user/signup", {
+    const response = await fetch("/api/user/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
@@ -33,5 +33,5 @@ export function useSignup() {
     }
   };
 
-  return { signup, isLoading, error };
+  return { login, isLoading, error };
 }
