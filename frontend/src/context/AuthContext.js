e73677dacc2c7e43de1875b279b3e08b0be3d515
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-05-26 22:19:36 -0700

    check if user token cached on page reload

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 const AuthContext = createContext();
 
@@ -17,6 +17,14 @@ function AuthContextProvider({ children }) {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    if (user) {
+        dispatch({type: 'LOGIN', payload: user})
+    }
+  }, [])
 
   console.log('AuthContext state: ', state)
 
