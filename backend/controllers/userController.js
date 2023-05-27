39f0f67a1027cb9499a93a9d5a8812ddf0d958d8
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-05-26 21:16:04 -0700

    json web tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,19 +1,41 @@
 // models
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+function createToken(_id) {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" }); // note: store correct secret token in .env
+}
 
 // login user
 async function loginUser(req, res) {
-  res.json({ msg: "login user" });
+
+  console.log('request body',req.body)
+
+  const {email, password} = req.body
+  try {
+    const user = await User.login(email, password);
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token });
+
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 }
 
 // sign up user
 async function signupUser(req, res) {
-
   const { email, password } = req.body;
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
